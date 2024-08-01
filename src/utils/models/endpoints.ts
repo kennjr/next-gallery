63d5954ed1de0a28e0aed04bc8858a90e9946b4e
kenjr <kenedy@myhechar.pro>
2024-08-01 19:13:43 +0300

    Endpoints.ts - file with all endpoint strings

---
+++ src/utils/models/endpoints.ts
@@ -0,0 +1,4 @@
+export const BASE_API_ENDPOINT = "https://api.unsplash.com"
+export const RANDOM_PHOTO_ENDPOINT = `${BASE_API_ENDPOINT}/photos/random`
+export const SEARCH_PHOTOS_ENDPOINT = `${BASE_API_ENDPOINT}/search/photos`
+export const USER_ENDPOINT = `${BASE_API_ENDPOINT}/users`
